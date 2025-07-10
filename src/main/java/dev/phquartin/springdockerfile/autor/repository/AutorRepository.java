298573867764715e8b2b045cb546229d79c7f512
phquartin <phquartinmatos@sempreceub.com>
2025-07-10 17:38:38 -0300

    feat: adiciona métodos de busca, listagem e deleção para entidade Autor e atualiza repositório

--- src/main/java/dev/phquartin/springdockerfile/autor/repository/AutorRepository.java
+++ src/main/java/dev/phquartin/springdockerfile/autor/repository/AutorRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AutorRepository extends JpaRepository<Autor, Long> {
+    Autor findByNomeContains(String nome);
 }
--- src/main/java/dev/phquartin/springdockerfile/autor/service/AutorService.java
+++ src/main/java/dev/phquartin/springdockerfile/autor/service/AutorService.java
@@ -3,7 +3,10 @@ package dev.phquartin.springdockerfile.autor.service;
 import dev.phquartin.springdockerfile.autor.model.Autor;
 import dev.phquartin.springdockerfile.autor.repository.AutorRepository;
 import dev.phquartin.springdockerfile.autor.validation.AutorValidation;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AutorService {
@@ -16,6 +19,22 @@ public class AutorService {
     public void save(Autor autor) {
         AutorValidation.validate(autor);
         autorRepository.save(autor);
+    }
+
+    public List<Autor> findAll() {
+        return autorRepository.findAll();
+    }
+
+    public Autor findById(Long id){
+        return autorRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Autor nao encontrado"));
+    }
+    public Autor findByNome(String nome){
+        return autorRepository.findByNomeContains(nome);
+    }
+
+    public void deleteById(Long id) {
+        findById(id);
+        autorRepository.deleteById(id);
     }
 
 }
