4c4e79de0762b623308f77786097129beca31aeb
phquartin <phquartinmatos@sempreceub.com>
2025-07-10 17:39:19 -0300

    feat: adiciona construtores, getters e setters para entidade Livro

--- src/main/java/dev/phquartin/springdockerfile/livro/model/Livro.java
+++ src/main/java/dev/phquartin/springdockerfile/livro/model/Livro.java
@@ -23,4 +23,45 @@ public class Livro {
     @Enumerated(EnumType.STRING)
     private Categoria categoria;
 
+    public Livro() {
+    }
+
+    public Livro(Long id, String titulo, Autor autor, Categoria categoria) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.categoria = categoria;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 }
