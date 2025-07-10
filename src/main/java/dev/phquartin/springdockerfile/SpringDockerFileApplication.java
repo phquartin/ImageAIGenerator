9f01af25d80e87fcd309cbc385084311744c0718
phquartin <phquartinmatos@sempreceub.com>
2025-07-10 16:42:04 -0300

    init: inicializa projeto com configuração básica do Spring Boot.

---
+++ src/main/java/dev/phquartin/springdockerfile/SpringDockerFileApplication.java
@@ -0,0 +1,13 @@
+package dev.phquartin.springdockerfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDockerFileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/phquartin/springdockerfile/SpringDockerFileApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.phquartin.springdockerfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerFileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
