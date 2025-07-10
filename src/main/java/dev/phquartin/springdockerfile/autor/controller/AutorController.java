8c29a488d94bb77b5ebfac5c154c44e6a22e4eb7
phquartin <phquartinmatos@sempreceub.com>
2025-07-10 17:44:08 -0300

    feat: adiciona controller para entidade Autor com métodos de CRUD

---
+++ src/main/java/dev/phquartin/springdockerfile/autor/controller/AutorController.java
@@ -0,0 +1,42 @@
+package dev.phquartin.springdockerfile.autor.controller;
+
+import dev.phquartin.springdockerfile.autor.model.Autor;
+import dev.phquartin.springdockerfile.autor.service.AutorService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/autor")
+public class AutorController {
+
+    private final AutorService autorService;
+    public AutorController(AutorService autorService) {
+        this.autorService = autorService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<String> create(@RequestBody Autor autor) {
+        autorService.save(autor);
+        return ResponseEntity.ok("Autor salvo com sucesso");
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Iterable<Autor>> findAll() {
+        return ResponseEntity.ok(autorService.findAll());
+    }
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Autor> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(autorService.findById(id));
+    }
+    @GetMapping("/nome/{nome}")
+    public ResponseEntity<Autor> findByNome(@PathVariable String nome) {
+        return ResponseEntity.ok(autorService.findByNome(nome));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        autorService.deleteById(id);
+        return ResponseEntity.ok("Autor deletado com sucesso");
+    }
+
+}
